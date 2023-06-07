7f2344d5f8fa192c03b6b908cd4a7f8f65605de9
SoravitPete <esterpiano.official@gmail.com>
2023-06-07 10:48:46 +0700

    find many to see all user.

--- script.ts
+++ script.ts
@@ -3,13 +3,18 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-    },
-  })
-  console.log(user)
+  // create user
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //   },
+  // })
+  // console.log(user)
+
+  //see all user
+  const users = await prisma.user.findMany()
+  console.log(users)
 }
 
 main()
