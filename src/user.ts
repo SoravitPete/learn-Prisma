f58978f1d54030e9f39fb4679c52e769af72033b
SoravitPete <esterpiano.official@gmail.com>
2023-06-08 15:59:23 +0700

    Add update and delete method

--- src/user.ts
+++ src/user.ts
@@ -1,62 +1,117 @@
 import { PrismaClient } from '@prisma/client'
-import express, {NextFunction, Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import * as Joi from "joi";
 
 const prisma = new PrismaClient()
 
 const userRoute = express.Router()
 
-userRoute.get('/', (req: Request, res: Response) => {
-    console.log('Test1')
-    res.send("Test1")
+userRoute.get('/test', (req: Request, res: Response) => {
+  console.log('Test1')
+  res.send("Test1")
 })
 
-userRoute.get('/all', async (req: Request, res: Response) => {
-    const users = await prisma.user.findMany()
-    console.log(users)
-    res.send(users)
+userRoute.get('/', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany()
+  console.log(users)
+  res.send(users)
+})
+
+userRoute.put('/', async (req: Request, res: Response) => {
+  try {
+		const data_name = req.body.name
+		const data_email = req.body.email
+		const schema = Joi.object({
+			name: Joi.string().alphanum().max(30).required(),
+			email: Joi.string().email().required()
+		})
+		var validate = schema.validate({ name: req.body.name, email: req.body.email })
+		if (!validate.error) {
+			try {
+				const updateUser = await prisma.user.update({
+					where: {
+						email: data_email
+					},
+					data: {
+						name: data_name
+					}
+				})
+				console.log(updateUser)
+				res.send(updateUser)
+			} catch (err) {
+        throw new Error('invalid email')
+			}
+		} else {
+			console.log("email or user name is wrong format")
+			res.send('email or user name is wrong format')
+		}
+  } catch (error) {
+    res.status(404).json({ message: 'error', error })
+  }
+})
+
+userRoute.delete('/:id', async (req: Request, res: Response) => {
+	try{
+		const schema = Joi.object({
+			id: Joi.number().integer(),
+		})
+		var test = schema.validate({ id: req.params.id })
+		if (!test.error) {
+			const deleteUser = await prisma.user.delete({
+				where: {
+					id: parseInt(req.params.id),
+				}
+			})
+			res.send(deleteUser)
+		} else {
+			res.status(404).json({ massgae: 'wrong format.'})
+		}
+	} catch (err) {
+		res.status(404).json({ massage: 'Cannot delete by this id'})
+	}
 })
 
 userRoute.post('/', async (req: Request, res: Response) => {
-    const schema = Joi.object({
-        name : Joi.string().required(),
-        email : Joi.string().email().required()
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required()
+  })
+  var test = schema.validate({ name: req.body.name, email: req.body.email })
+  if (!test.error) {
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+      },
     })
-    var test = schema.validate({name : req.body.name, email : req.body.email})
-    if (!test.error){
-      const user = await prisma.user.create({
-          data: {
-            name: req.body.name,
-            email: req.body.email,
-          },
-        })
-      console.log(user)
-      res.send(user)
-    }
-    else{
-      console.log("error na kub")
-      res.json({massage : "error na kub"})
-    }
+    console.log(user)
+    res.send(user)
+  }
+  else {
+    console.log("error na kub")
+    res.json({ massage: "error na kub" })
+  }
 
-    try {
-      await schema.validateAsync({
-        name : req.body.name, email : req.body.email
-      })
-    }
-    catch (err) {
-      reportError({massage: "error na kub"})
-    }
-    
+  try {
+    await schema.validateAsync({
+      name: req.body.name, email: req.body.email
+    })
+  } catch (err) {
+    reportError({ massage: "error na kub" })
+  }
 })
 
 userRoute.get('/:id', async (req: Request, res: Response) => {
-    const find_ID = await prisma.user.findUnique({
-        where : {
-          id: parseInt(req.params.id)
-        }
-      })
-      console.log(find_ID)
-    res.send(find_ID)
+  const find_ID = await prisma.user.findUnique({
+    where: {
+      id: parseInt(req.params.id)
+    },
+    include: {
+      posts: true
+    }
+  })
+  console.log(find_ID)
+  res.send(find_ID)
 })
 
 export default userRoute
